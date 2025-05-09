c81b00cfbde3783ddabc5d034f27f44db97f6960
oliann97 <111231726+oliann97@users.noreply.github.com>
2025-05-09 18:22:30 +0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,25 +1,15 @@
 fx_version "cerulean"
-
-description "Basic React (TypeScript) & Lua Game Scripts Boilerplate"
-author "Project Error"
+description "RedM 上下文菜单系统"
+author "oliann97"
 version '1.0.0'
-repository 'https://github.com/project-error/fivem-react-boilerplate-lua'
-
 lua54 'yes'
-
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
 games {
   "gta5",
   "rdr3"
 }
-
+client_script "client/menu.lua"
 ui_page 'web/build/index.html'
-
-client_script "client/**/*"
-shared_script {
-  '@ox_lib/init.lua'
-}
 files {
 	'web/build/index.html',
 	'web/build/**/*',
